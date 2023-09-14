2901ef579f06d28428775c9f95571a89fb8360d3
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-09-14 15:19:00 +0200

    feat: Address class created

--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace csharp_lista_indirizzi
+﻿using System.Runtime.CompilerServices;
+using static System.Console;
+namespace csharp_lista_indirizzi
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // 
+            List<Address> favouritesBooks = new List<Address>();
         }
     }
 }
